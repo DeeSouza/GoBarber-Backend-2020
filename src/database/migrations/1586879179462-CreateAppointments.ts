057c76b649cd33004d7fac91c8be3cbe5bbee62b
Diego Souza <dee-souza@hotmail.com>
2020-04-14 17:26:52 -0300

    :art: Change repository and model pattern typeorm and routes

--- src/database/migrations/1586879179462-CreateAppointments.ts
+++ src/database/migrations/1586879179462-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1586879179462
 						type: 'varchar',
 						isPrimary: true,
 						generationStrategy: 'uuid',
+						default: 'uuid_generate_v4()',
 					},
 					{
 						name: 'provider',
