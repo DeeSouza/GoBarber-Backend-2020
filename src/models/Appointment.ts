85a8721d8813d8534f494edc3c565ee3ccd15fe0
Diego Souza <dee-souza@hotmail.com>
2020-04-14 17:05:26 -0300

    :art: Change model to pattern typeorm

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,12 +1,15 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('appointments')
 class Appointment {
+	@PrimaryGeneratedColumn('uuid')
+	id: string;
+
+	@Column('varchar')
 	provider: string;
 
+	@Column('timestamp with time zone')
 	date: Date;
-
-	constructor({ provider, date }: Omit<Appointment, 'id'>) {
-		this.provider = provider;
-		this.date = date;
-	}
 }
 
 export default Appointment;
