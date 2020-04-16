506f0d774f38d102b64d8a3277fec9dc509cceda
Diego Souza <dee-souza@hotmail.com>
2020-04-16 12:15:33 -0300

    :art: Create route user and service user create

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -15,14 +15,14 @@ appointmentsRouter.get('/', async (request, response) => {
 });
 
 appointmentsRouter.post('/', async (request, response) => {
-	const { provider, date } = request.body;
+	const { provider_id, date } = request.body;
 	const parsedDate = parseISO(date);
 
 	const createAppointment = new CreateAppointmentService();
 
 	try {
 		const appointment = await createAppointment.execute({
-			provider,
+			provider_id,
 			date: parsedDate,
 		});
 
