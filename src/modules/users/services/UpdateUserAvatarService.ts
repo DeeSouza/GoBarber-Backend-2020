42f1843e431285cd722c8eb3443141165f9d9ce5
Diego Souza <dee-souza@hotmail.com>
2020-05-21 14:04:04 -0300

    :white_check_mark: Create tests create user and authenticate user

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -32,14 +32,9 @@ class UpdateUserAvatarService {
 		}
 
 		if (user.avatar) {
-			const userAvatarFilePath = path.join(
-				uploadConfig.directory,
-				user.avatar,
-			);
+			const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
 
-			const userAvatarFileExists = await fs.promises.stat(
-				userAvatarFilePath,
-			);
+			const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
 
 			if (userAvatarFileExists) {
 				await fs.promises.unlink(userAvatarFilePath);
