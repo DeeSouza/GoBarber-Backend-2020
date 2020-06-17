3ea4d34d7de9f1168ab6a7a27768307752f165f8
Diego Souza <dee-souza@hotmail.com>
2020-06-17 13:00:32 -0300

    :art: Validation fields request body and params api

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '../controllers/ProvidersController';
@@ -16,9 +17,19 @@ providersRouter.get('/', providersController.index);
 providersRouter.get(
 	'/:provider_id/month-availability',
 	providerMonthAvailabilityController.index,
+	celebrate({
+		[Segments.PARAMS]: {
+			provider_id: Joi.string().uuid().required(),
+		},
+	}),
 );
 providersRouter.get(
 	'/:provider_id/day-availability',
+	celebrate({
+		[Segments.PARAMS]: {
+			provider_id: Joi.string().uuid().required(),
+		},
+	}),
 	providerDayAvailabilityController.index,
 );
 
